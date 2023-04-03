7f687dbd534cb83b11bd27fd13bf1052eaf9d6a3
taewoo1113 <dlehdtls79@naver.com>
2023-04-03 13:40:34 +0900

    Javascript

--- HTML_CSS_JavaScript_ex/Javascript/js/change.js
+++ HTML_CSS_JavaScript_ex/Javascript/js/change.js
@@ -1,6 +1,9 @@
 var heading = document.querySelector("#heading2");
 heading.onclick = function(){
     var reply = confirm("글자색을 파란색으로 바꾸시겠습니까?")
-    console.log(reply);
-    heading.style.color = "blue"
+    
+    if (reply==true){
+        console.log(reply);
+        heading.style.color = "blue" 
+    }
 }
